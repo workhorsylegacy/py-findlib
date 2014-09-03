dbda94457b0310559a3828caa331cc368631ff0e
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2014-09-03 11:15:04 -0700

    Moved client and server code into one module.

--- cache_file_change_date.py
+++ cache_file_change_date.py
@@ -94,6 +94,31 @@ class CacheFileChangeDateServer(Server):
 
 		return False
 
+class CacheFileChangeDateClient(object):
+	def has_file_changed(self, file_name):
+		self._connect()
+
+		# Send a request to cache a file change date
+		data = {'request':'cache_file', 'file':file_name}
+		data = pickle.dumps(data)
+		self.sock.sendall(data)
+
+		# Get the response that says if it has changed or not since the last check
+		result = pickle.loads(self.sock.recv(1024))
+
+		self._disconnect()
+
+		return result
+
+	def _connect(self):
+		# Connect to the server
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.connect(('localhost', 9000))
+
+	def _disconnect(self):
+		# Disconnect from the server
+		self.sock.close()
+
 if __name__ == '__main__':
 	logging.basicConfig(level=logging.DEBUG)
 	server = CacheFileChangeDateServer('0.0.0.0', 9000)
--- client.py
+++ client.py
@@ -1,32 +1,7 @@
 
 
-import socket
-import pickle
+from cache_file_change_date import *
 
-class CacheFileChangeDateClient(object):
-	def has_file_changed(self, file_name):
-		self._connect()
-
-		# Send a request to cache a file change date
-		data = {'request':'cache_file', 'file':file_name}
-		data = pickle.dumps(data)
-		self.sock.sendall(data)
-
-		# Get the response that says if it has changed or not since the last check
-		result = pickle.loads(self.sock.recv(1024))
-
-		self._disconnect()
-
-		return result
-
-	def _connect(self):
-		# Connect to the server
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.sock.connect(('localhost', 9000))
-
-	def _disconnect(self):
-		# Disconnect from the server
-		self.sock.close()
 
 if __name__ == '__main__':
 	client = CacheFileChangeDateClient()
@@ -34,7 +9,7 @@ if __name__ == '__main__':
 	r = client.has_file_changed('/home/matt/Projects/py-cache-file-change-dates/client.py')
 	print(r)
 
-	r = client.has_file_changed('/home/matt/Projects/py-cache-file-change-dates/server.py')
+	r = client.has_file_changed('/home/matt/Projects/py-cache-file-change-dates/cache_file_change_date.py')
 	print(r)
 
 
