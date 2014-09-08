39bc5cbca0488550e372427927aa390526c4cda1
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2014-09-08 10:34:56 -0700

    Added example.

---
+++ example.py
@@ -0,0 +1,35 @@
+
+
+import findlib
+
+
+
+print('Looking for libpcre shared library ...')
+libs = findlib.get_shared_library('libpcre', 'ver.major == 8')
+print('    {0}'.format(libs))
+
+print('Looking for libpcre static library ...')
+libs = findlib.get_static_library('libpcre', 'ver.major == 8')
+print('    {0}'.format(libs))
+
+print('Looking for libpcre header file ...')
+libs = findlib.get_header_file('libpcre', 'ver.major == 8')
+print('    {0}'.format(libs))
+
+
+# Print to stdout
+findlib.run_print('uptime')
+
+# Or return stdout to variable
+result = findlib.run_and_get_stdout('uptime')
+print(result)
+
+# Find the full path of gcc
+result = findlib.program_paths('gcc')
+print(result)
+
+
+# Recursively expand an environmental variable
+result = findlib.expand_envs('$PATH')
+print(result)
+
